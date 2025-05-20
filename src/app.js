554ca7a7ee16a37e0e7fc1e0b94c54492a0f8fef
Aman <amankunalbharti2003@gmail.com>
2025-05-20 12:08:35 +0530

    multiple route handling and use of next

--- src/app.js
+++ src/app.js
@@ -2,23 +2,38 @@ const express= require('express');
 
 const app=express();
 
-app.get('/user',(req,res)=>{
-    res.send("get data of user!!");
-})
+app.get('/user',(req,res,next)=>{
+    console.log("Middleware 1");
+    // res.send("get data of user1!!");
+    next();
+},
+(req,res,next)=>{
+    console.log("Middleware 2");
+    next();
+    res.send("get data of user2!!");
+},
+(req,res,next)=>{
+    console.log("Middleware 3");
+    // next();
+    res.send("get data of user3!!");
+},
+)
 
-app.post('/user',(req,res)=>{
-    //DB logic
-    res.send("post data of user in the database!!");
-});
-
-app.delete('/user',(req,res)=>{
-    res.send("delete data of user from the database!!");
-})
 
 
-app.use("/",(req,res)=>{
-    res.send('Welcome Mr. Aman!');
-})
+// app.post('/user',(req,res)=>{
+//     //DB logic
+//     res.send("post data of user in the database!!");
+// });
+
+// app.delete('/user',(req,res)=>{
+//     res.send("delete data of user from the database!!");
+// })
+
+
+// app.use("/",(req,res)=>{
+//     res.send('Welcome Mr. Aman!');
+// })
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
